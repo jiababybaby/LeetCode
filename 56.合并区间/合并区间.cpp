54997694a45fff7ca33417429614c0e131427dae
lenovo <1520170162@qq.com>
2020-02-24 12:31:07 +0800

    2.24

--- 56.合并区间/合并区间.cpp
+++ 56.合并区间/合并区间.cpp
@@ -1,3 +1,6 @@
+#include <vector>
+#include <Algorithm>
+using namespace std;
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
---
+++ CPPTEST/string.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string s1,s2;
+    s1="abcd";
+    s2="1234";
+    cout<<s1+s2;
+}
